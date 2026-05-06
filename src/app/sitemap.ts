54ec7f76813b1af5486fcd373858c6cf8368074c
guhara1 <88smartbro88@gmail.com>
2026-05-06 19:04:55 +0900

    Add reviews page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,6 @@ const siteUrl = "https://massagepick.netlify.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date();
-  const urls = ["/", ...getPrimaryRegions().flatMap((region) => [pathFor(region), ...getDistricts(region).flatMap((district) => [pathFor(region, district), ...getNeighborhoods(region, district).map((dong) => pathFor(region, district, dong))])])];
+  const urls = ["/", "/reviews/", ...getPrimaryRegions().flatMap((region) => [pathFor(region), ...getDistricts(region).flatMap((district) => [pathFor(region, district), ...getNeighborhoods(region, district).map((dong) => pathFor(region, district, dong))])])];
   return urls.map((url) => ({ url: new URL(url, siteUrl).toString(), lastModified, changeFrequency: url === "/" ? "daily" : "weekly", priority: url === "/" ? 1 : 0.8 }));
 }
